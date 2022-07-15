d093e483c7455dbac7e5cf51dcae8df1de0ea164
thejayeshsoni <sonijayesh12345@gmail.com>
2022-07-15 15:47:11 +0530

    initial commit

---
+++ sample.js
@@ -0,0 +1,11 @@
+const _ = require("lodash");
+
+const members = [
+    { name: 'Rakesh Gupta', age: 20 },
+    { name: 'Yash Jangid', age: 40 },
+    { name: 'Firoz Khan', age: 41 },
+    { name: 'Amrit Srivastava', age: 17 },
+    { name: 'Chandraprakash Sharma' },
+    { name: 'Swpril Ahuja', age: 45 },
+    { name: 'Yogesh Khatri', age: 51 }
+];
